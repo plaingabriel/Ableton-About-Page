005a48ac532e03d62890b317b505c54815642d1e
Gabriel Prado <gaboalespana@gmail.com>
2024-05-27 09:01:37 +1000

    add NavBar

---
+++ src/const.js
@@ -0,0 +1,57 @@
+export const link = "https://www.ableton.com/en/";
+
+export const links = ["Live", "Push", "Note", "Link", "Shop", "Packs", "Help"];
+
+export const abletonLinks = [
+  {
+    name: "Blog",
+    url: "blog/",
+  },
+  {
+    name: "Ableton for the Classroom",
+    url: "classroom/",
+  },
+  {
+    name: "Ableton for Colleges and Universities",
+    url: "colleges-universities/",
+  },
+  {
+    name: "Certified Training",
+    url: "certified-training/",
+  },
+  {
+    name: "About Ableton",
+    url: "about/",
+  },
+  {
+    name: "Jobs",
+    url: "jobs/",
+  },
+  {
+    name: "Apprenticeships",
+    url: "ausbildung/",
+  },
+];
+
+export const abletonLearningLinks = [
+  {
+    title: "Loop",
+    body: "Watch Talks, Performances and Features from Ableton's Summit for Music Makers",
+    url: `${link}blog/categories/loop/`,
+  },
+  {
+    title: "Learning Music",
+    body: "Learn the fundamentals of music making right in your browser.",
+    url: "https://learningmusic.ableton.com",
+  },
+  {
+    title: "Learning Synths",
+    body: "Get started with synthesis using a web-based synth and accompanying lessons.",
+    url: "https://learningsynths.ableton.com/",
+  },
+  {
+    title: "Making Music",
+    body: "Some tips from 74 Creative Strategies for Electronic Producers.",
+    url: "https://makingmusic.ableton.com",
+  },
+];
